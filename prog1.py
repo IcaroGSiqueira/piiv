5e38e9e5fe7f66332dbbe020f125d02ac193816b
IcaroGS <icarogs1998@gmail.com>
2019-08-27 02:33:43 -0300

    prog1 atualizado

--- prog1.py
+++ prog1.py
@@ -1,3 +1,5 @@
 #primeiro programa em python
 #segunda linha do programa
 
+nome = input("Digite o seu nome: \n")
+print("Ola ", nome, "!")
---
+++ prog1_old.py
@@ -0,0 +1,5 @@
+#primeiro programa em python
+#segunda linha do programa
+
+nome = raw_input("Digite o seu nome: \n")
+print("Ola "+ nome + "!")
