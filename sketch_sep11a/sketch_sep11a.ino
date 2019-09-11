e1ebfeee74e2c6c5e61f00247a78c0419ff668d4
IcaroGS <icarogs1998@gmail.com>
2019-09-11 20:31:46 -0300

    Atualizando do git

--- sketch_sep11a/sketch_sep11a.ino
+++ sketch_sep11a/sketch_sep11a.ino
@@ -1,10 +1,12 @@
 int analogPin = 0; 
+int ledPin = 2;
 int val=0;
 float tensao=0;
  
 //configurando os pinos (executado apenas uma vez)
 void setup(){
- Serial.begin(9600); 
+ Serial.begin(9600);
+ pinMode(ledPin, OUTPUT);
 }
  
 //laço principal (executado infinitamente)
@@ -13,5 +15,9 @@ void loop() {
   val = analogRead(analogPin);   // lê o pino de entrada
   tensao = ((5.0 * val)/1023);
   Serial.println(tensao); // imprime o valor na porta serial
- 
+  if(tensao >= "2.5"){
+    pinMode(ledPin,HIGH);    
+  }else{
+    pinMode(ledPin,LOW);
+  }
 }
